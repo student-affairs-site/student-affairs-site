e61290977465b3f95304d825c29bb0b0eb892823
unknown <chima.okwuokei@pau.edu.ng>
2024-08-19 00:01:54 +0100

    [feat:] admin section for adding events & date

--- backend/src/controller/date.controller.ts
+++ backend/src/controller/date.controller.ts
@@ -8,3 +8,44 @@ export const getDate = async (req: Request, res: Response) => {
     res.status(StatusCodes.OK).json(date);
 
 };
+
+export const createDate = async (req: Request, res: Response) => {
+    const date = new Date(req.body);
+    try {
+        const newDate = await date.save();
+        res.status(201).json(newDate);
+    } catch (error) {
+        res.status(400).json({ message: (error as Error).message });
+    }
+};
+
+export const updateDate = async (req: Request, res: Response) => {
+    try {
+        const date = await Date.findByIdAndUpdate(req.params._id, req.body, { new: true });
+        if (!date) return res.status(404).json({ message: 'Date not found' });
+        res.json(date);
+    } catch (error) {
+        res.status(400).json({ message: (error as Error).message });
+    }
+};
+
+export const deleteDate = async (req: Request, res: Response) => {
+    try {
+        const date = await Date.findByIdAndDelete(req.params._id);
+        if (!date) return res.status(404).json({ message: 'Date not found' });
+        res.json({ message: 'Date deleted' });
+    } catch (error) {
+        res.status(500).json({ message: (error as Error).message });
+    }
+};
+
+// get Date by ID
+export const getDateById = async (req: Request, res: Response) => {
+    try {
+        const date = await Date.findById(req.params._id);
+        if (!date) return res.status(404).json({ message: 'Date not found' });
+        res.status(StatusCodes.OK).json(date);
+    } catch (error) {
+        res.status(500).json({ message: (error as Error).message });
+    }
+};
--- backend/src/routes/dateRoute.ts
+++ backend/src/routes/dateRoute.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { getDate } from "../controller/date.controller";
+import { getDate, createDate, updateDate, deleteDate, getDateById} from "../controller/date.controller";
 import authenticateTokenMiddleware from "../middleware/authenticateTokenMiddleware";
 
 const router = express.Router();
 
-router.route("/").get(getDate);
+router.route("/").get( getDate)
+                 .post(createDate);
+
+router.route('/:_id')
+                 .get( getDateById)
+                 .put( updateDate)
+                 .delete(deleteDate);
 
 export default router;
