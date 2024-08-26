9f38f8ce2e489af810ebdef8b079af6138ab4d5b
dominioncruz <nnadiukwumiracleike@gmail.com>
2024-08-26 12:58:52 +0100

    fix:updated mail config

--- backend/src/services/mail/transporter.ts
+++ backend/src/services/mail/transporter.ts
@@ -1,18 +1,18 @@
 import nodemailer from "nodemailer";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    auth: {
-        user: process.env.USER_MAIL as string,
-        pass: process.env.MAIL_PASS as string
-    },
-    tls: {
-        rejectUnauthorized: false
-    },
-    debug: true
+  host: "smtp.gmail.com",
+  port: 465,
+  auth: {
+    user: process.env.USER_MAIL as unknown as string,
+    pass: process.env.MAIL_PASS as unknown as string,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  debug: true,
 });
 
 export default transporter;
